1ca19b2ece14b8b789b07c5cb7883942f5e16cda
starrymm <zimingming1996@foxmail.com>
2023-06-18 16:57:00 +0800

    制作库文件

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "cJSON.h"
+#include "hello.h"
 
 void main() {
     printf("hello cmake\n");
     cJSON *root = cJSON_CreateObject();
+    printHello();
     return ;
 }
